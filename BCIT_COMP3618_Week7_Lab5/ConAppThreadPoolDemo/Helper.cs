8f9a5a7af78a6ee270060c495712d75449747827
Krzysztof Szczurowski <kszczurowski@absolute.com>
2018-02-24 17:04:24 -0800

    Adding Helper class implementation, refactoring ShowMyText static method;

---
+++ BCIT_COMP3618_Week7_Lab5/ConAppThreadPoolDemo/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using static System.Console;
+
+namespace ConAppThreadPoolDemo
+{
+    public class Helper
+    {
+        /// <summary>
+        /// Krzysztof Szczurowski - BCIT COMP 3618 Week 7 Lab 5
+        /// Helper class used to separed some operations from main application;
+        /// Repo: https://github.com/kriss3/BCIT_COMP3618_Week7_Lab5.git
+        /// </summary>
+        /// <param name="state"></param>
+        public static void ShowMyText(object state)
+        {
+            var myText = (string)state;
+            //Below should not be here but delegate has void returning function signature?
+            WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId} - {myText}");
+        }
+    }
+}
--- BCIT_COMP3618_Week7_Lab5/ConAppThreadPoolDemo/MyApp.cs
+++ BCIT_COMP3618_Week7_Lab5/ConAppThreadPoolDemo/MyApp.cs
@@ -1,15 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading;
+using static System.Console;
 
 namespace ConAppThreadPoolDemo
 {
     class MyApp
     {
+        /// <summary>
+        /// Krzysztof Szczurowski - BCIT COMP 3618 Week 7 Lab 5
+        /// Main applicaton to present working of ThreadPool class
+        /// Repo: https://github.com/kriss3/BCIT_COMP3618_Week7_Lab5.git
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            WaitCallback callback = new WaitCallback(Helper.ShowMyText);
+            ThreadPool.QueueUserWorkItem(callback, "Hello");
+            ThreadPool.QueueUserWorkItem(callback, "Hi");
+            ThreadPool.QueueUserWorkItem(callback, "Heya");
+            ThreadPool.QueueUserWorkItem(callback, "Goodbye");
+            Read();
         }
     }
 }
